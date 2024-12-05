8d5707cbb0cb8c8a925ccd8223f81d2e87fb020a
Thais Menezes <thais.mas@hotmail.com>
2024-12-05 10:01:36 -0300

    feat: adding limit of questions, restart button and score

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -16,48 +16,105 @@ struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
     
+    @State private var gameOver = false
+    
+    @State private var playingCount = 1
+    
+    @State private var score = 0
+    
     var body: some View {
         ZStack {
-            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
-                .ignoresSafeArea()
-            VStack(spacing: 30) {
-                VStack{
-                Text("Tap the flag of")
-                        .foregroundStyle(.white)
-                Text(countries[correctAnswer])
-                        .foregroundStyle(.white)
-                
-            }
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
             
-                ForEach(0..<3) { number in
-                    Button {
+            VStack{
+                
+                Spacer()
+                Text("Guess The Flag, Pepe")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundStyle(.white)
+                VStack(spacing: 15) {
+                    VStack{
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .foregroundStyle(Color(red: 0.76, green: 0.15, blue: 0.26))
+                            .font(.largeTitle.weight(.semibold))
+                        Text("Question number \(playingCount)")
                         
-                    } label: {
-                        Image(countries[number])
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Text("Score: \(score)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                
+                Button("Restart", systemImage: "arrow.clockwise") {
+                    reset()
+                }
+                .buttonStyle(.bordered)
+                
+                Spacer()
             }
+            .padding()
         }
         
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(score)")
         }
+        
+        .alert("Game Over", isPresented: $gameOver) {
+            Button("Start Again", action: reset)
+        } message: {
+            Text("You finished the game! Your final score was: \(score)")
+        }
+        
     }
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
-            scoreTitle = "Correct"
+            scoreTitle = "Correct! Congratsss baby"
+            score += 100
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong! The one you taped is the flag of \(countries[number])"
+            score = 0
         }
         
         showingScore = true
     }
     func askQuestion() {
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+            playingCount += 1
+        if playingCount <= 8 {
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+        } else {
+            gameOver = true
+        }
+    }
+    
+    func reset(){
+        score = 0
+        playingCount = 1
     }
 }
 
